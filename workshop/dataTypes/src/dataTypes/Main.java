50d9fef28eb901fcd2919e8dc449004e04d39a9c
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2023-04-07 02:14:45 +0100

    dataTypes

---
+++ workshop/dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,21 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Primitive Types
+		//boolean
+		//char
+		//byte
+		//short
+		//int
+		//long
+		//floot point
+		//double point
+		
+		
+		
+
+	}
+
+}
