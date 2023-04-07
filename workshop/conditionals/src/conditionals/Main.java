e1e7815a218ee508ab43796903eacfa451aadef7
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2023-04-07 11:48:34 +0100

    if else block

---
+++ workshop/conditionals/src/conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number =19;
+		
+		if (number > 21) {
+			System.out.println("bigger");
+		}else if (number < 20 ) {
+			System.out.println("smaller");
+		}
+		else {
+			System.out.println("equals");
+		}
+
+	}
+
+}
