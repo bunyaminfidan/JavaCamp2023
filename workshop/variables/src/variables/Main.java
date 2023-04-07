b8bf02732d5279d71285867df926ecdbe880ce5d
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2023-04-07 01:57:03 +0100

    splicing

---
+++ workshop/variables/src/variables/Main.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int studentCount = 12;
+		String message = "Student count : ";
+		
+		System.out.println(message +studentCount);
+
+	}
+
+}
