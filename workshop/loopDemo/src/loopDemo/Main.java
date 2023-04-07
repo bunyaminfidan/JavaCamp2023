b34ea340d7eef2a73c0b1fc77ab98c03336e3b38
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2023-04-07 13:15:15 +0100

    loop - for/while/do-while

---
+++ workshop/loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,38 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// For
+
+		for (int i = 1; i < 10; i++) {
+			System.out.println("Number of the loop = " + i);
+
+		}
+		System.out.println("The for loops is over");
+	
+	//While
+		int i = 1;
+		while (i< 10) {
+			System.out.println(i);
+			i++;
+			
+		}
+		
+		System.out.println("The while loops is over");
+		
+	//Do-While
+		
+		int j = 100;
+		do {
+				System.out.println(j);
+			j++;
+		} while (j<10);
+		System.out.println("The do-while loops is over");
+	}
+	
+
+	
+
+}
