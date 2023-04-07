1a09cb9c9496f81bfe30fe66f5a0e1517f5a8589
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2023-04-07 01:40:22 +0100

    Hello Java Workshop first commit

--- workshop/helloWorld2023/src/helloWorld2023/Main.java
+++ workshop/helloWorld2023/src/helloWorld2023/Main.java
@@ -3,7 +3,8 @@ package helloWorld2023;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World 2023!");
+		System.out.println("Hello Java 2023!");
+
 	}
 
 }
