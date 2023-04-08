52821ca77bc0ab7c7147230a696236a2ba799aab
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2023-04-08 13:04:02 +0100

    Customer Manager process

---
+++ workshop/classes/src/classes/CustomeManager.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class CustomeManager {
+
+	public void Add() {
+		System.out.println("Customer Added");
+	}
+
+	public void Update() {
+		System.out.println("Customer Updated");
+	}
+
+	public void Delete() {
+		System.out.println("Customer Removed");
+	}
+
+}
---
+++ workshop/classes/src/classes/Main.java
@@ -0,0 +1,14 @@
+package classes;
+import classes.CustomeManager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomeManager customeManager = new CustomeManager();
+		customeManager.Add();
+		customeManager.Update();
+		customeManager.Delete();
+		
+	}
+
+}
