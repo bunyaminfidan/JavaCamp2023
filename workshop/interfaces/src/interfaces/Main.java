a3e8bb9e0616744772fbe2ad58681ddda1fe8855
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2023-04-09 15:17:06 +0100

    syntax fixed

--- workshop/interfaces/src/interfaces/Main.java
+++ workshop/interfaces/src/interfaces/Main.java
@@ -3,7 +3,7 @@ package interfaces;
 public class Main {
 
 	public static void main(String[] args) {
-		ICustomerDal customerDal = new OracleCustomerDal():
+		ICustomerDal customerDal = new OracleCustomerDal();
 
 	}
 
